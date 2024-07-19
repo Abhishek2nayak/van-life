5844f81e0a13f3d4f80ba087fd77ac7f8babf8cc
abhishek nayak <abhishek5nayak22@gmail.com>
2024-07-19 22:58:10 +0530

    fix styles issues

--- components/Header.jsx
+++ components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
                         className="login-icon"
                     />
                 </Link>
-                <button onClick={fakeLogOut}>X</button>
+                <button className='logout-btn' onClick={fakeLogOut}>LogOut</button>
             </nav>
         </header>
     )
--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -23,13 +23,16 @@ export default function VanDetail() {
 
             <div className="van-detail">
                 <img src={van.imageUrl} />
-                <i className={`van-type ${van.type} selected`}>
-                    {van.type}
-                </i>
-                <h2>{van.name}</h2>
-                <p className="van-price"><span>${van.price}</span>/day</p>
-                <p>{van.description}</p>
-                <button className="link-button">Rent this van</button>
+                <div>
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+
             </div>
 
         </div>
